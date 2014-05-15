058eed4ff2d79d30394fd29081aa9da17ff3223a
Vianey Villalobos <vvillalobos-zuniga14@smpanthers.org>
2014-05-15 11:49:35 -0700

    finished product?

--- collatz.js
+++ collatz.js
@@ -1,1 +1,21 @@
-console.log("Vianey's Collatz");
+console.log("Vianey's Collatz \n");
+
+var collatz = function (x) {
+	var length = 0
+	console.log(x);
+	while(x > 1){
+	   if (x % 2 == 0) {
+	  	x = x / 2;
+	   }
+	   else {
+		x = 3*x + 1;
+	   }
+	   console.log(x);
+	   length += 1
+	};
+	return length
+};
+
+var start = 59
+var length = collatz(start);
+console.log("Collatz(59) has "  + length  + " items")
